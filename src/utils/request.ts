40f69c191aaaa45d971a4a9c06ee4de1b0f9e39e
Leo <solar_system_2025@outlook.com>
2025-02-18 22:08:20 +0800

    refactor(utils): 简化 request.ts 文件结构
    - 移除了未使用的 AxiosRequestConfig 和 AxiosResponse 类型导入
    - 删除了未使用的请求拦截器和响应拦截器代码

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import type { AxiosInstance } from 'axios'
 
 // 创建 axios 实例
 const service: AxiosInstance = axios.create({
@@ -9,35 +9,5 @@ const service: AxiosInstance = axios.create({
     'Content-Type': 'application/json'
   }
 })
-
-// 请求拦截器
-service.interceptors.request.use(
-  (config: AxiosRequestConfig<any>) => {
-    // 在发送请求之前做些什么
-    // 例如：添加 token
-    // if (token) {
-    //   config.headers['Authorization'] = `Bearer ${token}`
-    // }
-    return config
-  },
-  (error) => {
-    // 处理请求错误
-    console.error('Request error:', error)
-    return Promise.reject(error)
-  }
-)
-
-// 响应拦截器
-service.interceptors.response.use(
-  (response: AxiosResponse) => {
-    // 处理响应数据
-    return response.data
-  },
-  (error) => {
-    // 处理响应错误
-    console.error('Response error:', error)
-    return Promise.reject(error)
-  }
-)
 
 export default service
